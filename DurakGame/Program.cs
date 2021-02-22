78d346d20234f0a6d5e11c145329d1bd37cb130b
zogry aadith <zogryaadith007@outlook.com>
2021-02-22 17:35:19 -0500

    hello world test

--- DurakGame/Program.cs
+++ DurakGame/Program.cs
@@ -10,6 +10,8 @@ namespace DurakGame
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!!!");
+            Console.ReadKey();
         }
     }
 }
