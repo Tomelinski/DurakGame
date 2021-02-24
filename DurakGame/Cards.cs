d3ca97c4c01af4a6fcf5e9957edc65db586080e3
Menu <syncrow123@gmail.com>
2021-02-24 11:13:17 -0500

    Added copyTo and Clone

--- DurakGame/Cards.cs
+++ DurakGame/Cards.cs
@@ -6,8 +6,29 @@ using System.Threading.Tasks;
 
 namespace DurakGame
 {
-    class Cards
-    {
-        // Chicken Nuggets
-    }
+   public class Cards : List<Card>, ICloneable
+   {
+      /// <summary>
+      /// Utility method for copying card instances into another Cards
+      /// instance—used in Deck.Shuffle(). This implementation assumes that
+      /// source and target collections are the same size.
+      /// </summary>
+      public void CopyTo(Cards targetCards)
+      {
+         for (int index = 0; index < this.Count; index++)
+         {
+            targetCards[index] = this[index];
+         }
+      }
+
+      public object Clone()
+      {
+         Cards newCards = new Cards();
+         foreach (Card sourceCard in this)
+         {
+            newCards.Add((Card)sourceCard.Clone());
+         }
+         return newCards;
+      }
+   }
 }
