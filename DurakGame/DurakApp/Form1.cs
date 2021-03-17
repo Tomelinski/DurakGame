9fe6e9c8613ad93575da47a4107c7c190caa9879
zogry aadith <zogryaadith007@outlook.com>
2021-03-17 16:28:58 -0400

    added image resources

--- DurakGame/DurakApp/Form1.cs
+++ DurakGame/DurakApp/Form1.cs
@@ -16,5 +16,15 @@ namespace DurakApp
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DeckBox_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
